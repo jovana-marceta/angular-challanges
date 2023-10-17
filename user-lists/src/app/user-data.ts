588d612aebcd860f34e31c0e980c08c5217c5897
jovana-marceta <jmarcetafedev@gmail.com>
2023-10-17 15:46:19 +0200

    Renaming structural directives challenge

---
+++ user-lists/src/app/user-data.ts
@@ -0,0 +1,12 @@
+import { User } from "./user";
+
+export const USERS: User[] = [
+    { name: 'Emily Johnson', age: 20 },
+    { name: 'Michael Davis', age: 10 },
+    { name: 'Sophia Martinez', age: 15 },
+    { name: 'Daniel Brown', age: 18 },
+    { name: 'Olivia Wilson', age: 25 },
+    { name: 'Ethan Miller', age: 39 },
+    { name: 'Ava Jones', age: 40 },
+    { name: 'Liam Smith', age: 50 }
+];
